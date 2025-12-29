6ce96b97546daf32bf3904e995ea8c9e24a0ebc0
armandoschianodicola <armando.schianodicola@gmail.com>
2025-12-29 18:16:32 +0100

    refactor main

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from routers.rag import router as rag_router
 from services.rag_pipeline import preload_rag
 
-app = FastAPI(title="HT AI Backend")
 
-@app.on_event("startup")
-async def startup_event():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup logic
     await preload_rag()
+    yield
+
+backend_app = FastAPI(title="HT AI Backend")
 
 # register routes
-app.include_router(rag_router, prefix="/rag")
+backend_app.include_router(rag_router, prefix="/rag")
